27f0b2d016b6aef50dcf2d93cbf005db52eddec7
Matheus <matheusamg@gmail.com>
2025-04-14 09:51:04 -0300

    primeira tentativa ok

--- calculadora.v
+++ calculadora.v
@@ -1,10 +1,14 @@
 module calculadora(
-  input   [7:0] entrada_A,
-  input   [7:0] entrada_B,
-  input   [2:0] codigo,
-  output  [7:0] saida
+  input wire [7:0] entrada_A,
+  input wire [7:0] entrada_B,
+  input wire [2:0] codigo,
+  output wire [7:0] saida
 );
 
-// insira seu código aqui
+assign saida = (codigo == 3'b001) ? (entrada_A) :
+               (codigo == 3'b010) ? (entrada_B) :
+               (codigo == 3'b011) ? (entrada_A + entrada_B) :
+               (codigo == 3'b100) ? (entrada_A - entrada_B) :
+               8'b0;
 
 endmodule
